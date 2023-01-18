b26c4aeb2f397985bfb18cdfc2d66a8597a5d2be
Titossaniti <tristan.erard@gmail.com>
2023-01-18 10:11:44 +0100

    commit before SWIPER tests

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { GameAPIService } from 'src/app/services/game-api.service';
+
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent{
-
+export class HeaderComponent implements OnInit{
+  constructor(private http: HttpClient, private imageService: GameAPIService) { }
+  ngOnInit(): void {
+    this.imageService.getImages().subscribe(data => {
+      this.images = data; 
+  }
+  }
+  
 }
