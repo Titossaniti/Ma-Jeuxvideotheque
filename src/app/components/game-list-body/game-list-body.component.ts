207a688b06db3ad8446d6c4a057eb3af4f697214
Titossaniti <tristan.erard@gmail.com>
2023-01-25 16:06:08 +0100

    functionnal filters for studios

--- src/app/components/game-list-body/game-list-body.component.ts
+++ src/app/components/game-list-body/game-list-body.component.ts
@@ -19,24 +19,29 @@ interface Item {
   templateUrl: './game-list-body.component.html',
   styleUrls: ['./game-list-body.component.scss']
 })
+
 export class GameListBodyComponent implements OnInit {
-  data:any;
+  data: any;
   nbOfGame: any;
   currentPage = 1;
   itemsPerPage = 4;
-  availableStudios: any[]=[];
+  filterByStudio: any;
+  uniqueStudios:any;
+  showFilters = false;
+  selectedStudios: string[] = [];
 
-  constructor(private gameService: GameAPIService) {}
+constructor(private gameService: GameAPIService) {}
 
   ngOnInit() {
     this.gameService.getDataFromApi().subscribe(data => {
       this.data = data;
       this.nbOfGame = this.data.length;
-      this.availableStudios = Array.from(new Set(this.data.map((item: Item) => item.studio)));
+      this.uniqueStudios = Array.from(new Set(this.data.map((item:Item) => item.studio)));
+      this.uniqueStudios.sort();
     });
   }
 
-//Sorting API objects by year
+//Trier par année de sortie
   sortByYear(sortOrder: string) {
     if (sortOrder === 'asc') {
       this.data.sort((a:any, b:any) => a.year - b.year);
@@ -44,7 +49,7 @@ export class GameListBodyComponent implements OnInit {
       this.data.sort((a:any, b:any) => b.year - a.year);
     }
   }
-//same but by name
+//Trier par nom (A à Z et Z à A)
   sortByName(sortOrder: string) {
     if (sortOrder === 'asc') {
       this.data.sort((a:any, b:any) => (a.name > b.name) ? 1 : -1);
@@ -53,27 +58,41 @@ export class GameListBodyComponent implements OnInit {
     }
   }
 
-//filter
+// Fonction pour obtenir les éléments de la page courante, slice pour retirer les éléments non nécéssaires
+get items(): Item[] {
+  let filteredData = this.data;
+  if (this.selectedStudios.length > 0) {
+      filteredData = this.data.filter((item:Item) => this.selectedStudios.includes(item.studio));
+  }
+  const start = (this.currentPage - 1) * this.itemsPerPage;
+  return filteredData.slice(start, start + this.itemsPerPage);
+}
 
-filterByStudio(studio: string) {
-  this.data = this.data.filter((item: Item) => item.studio === studio);
+
+//filtrer
+filterBySelectedStudio(studio: string) {
+  this.filterByStudio = studio;
+  this.currentPage = 1;
+}
+
+//fonction  pour mettre à jour le tableau des studios sélectionnés lorsqu'une case est cochée et décochée
+updateSelectedStudios(studio: string, checkedInput: HTMLInputElement) {
+  if (checkedInput.checked) {
+      this.selectedStudios.push(studio);
+  } else {
+      this.selectedStudios = this.selectedStudios.filter(s => s !== studio);
+  }
   this.currentPage = 1;
 }
 
 
 
-  // Fonction pour obtenir les éléments de la page courante
-  get items(): Item[] {
-    const start = (this.currentPage - 1) * this.itemsPerPage;
-    return this.data.slice(start, start + this.itemsPerPage);
-  }
 
-  // Fonction pour aller à la page suivante
+// Fonction pour aller à la page suivante
   nextPage() {
     this.currentPage++;
   }
-
-  // Fonction pour aller à la page précédente
+// Fonction pour aller à la page précédente
   previousPage() {
     this.currentPage--;
   }
