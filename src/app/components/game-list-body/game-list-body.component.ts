17dde3e2250cd875b425fd74587f4bdadb20a551
Titossaniti <tristan.erard@gmail.com>
2023-01-26 12:09:02 +0100

    all filters working

--- src/app/components/game-list-body/game-list-body.component.ts
+++ src/app/components/game-list-body/game-list-body.component.ts
@@ -25,10 +25,16 @@ export class GameListBodyComponent implements OnInit {
   nbOfGame: any;
   currentPage = 1;
   itemsPerPage = 4;
-  filterByStudio: any;
-  uniqueStudios:any;
   showFilters = false;
+
+  uniqueStudios:any;
   selectedStudios: string[] = [];
+
+  uniqueYears:any;
+  selectedYears: string[] = [];
+
+  uniqueSupports:any;
+  selectedSupports: string[] = [];
 
 constructor(private gameService: GameAPIService) {}
 
@@ -36,11 +42,17 @@ constructor(private gameService: GameAPIService) {}
     this.gameService.getDataFromApi().subscribe(data => {
       this.data = data;
       this.nbOfGame = this.data.length;
+
       this.uniqueStudios = Array.from(new Set(this.data.map((item:Item) => item.studio)));
       this.uniqueStudios.sort();
+
+      this.uniqueYears = Array.from(new Set(this.data.map((item:Item) => item.year)));
+      this.uniqueYears.sort();
+
+      this.uniqueSupports = Array.from(new Set(this.data.map((item:Item) => item.support)));
+      this.uniqueSupports.sort();
     });
   }
-
 //Trier par année de sortie
   sortByYear(sortOrder: string) {
     if (sortOrder === 'asc') {
@@ -57,25 +69,22 @@ constructor(private gameService: GameAPIService) {}
       this.data.sort((a:any, b:any) => (a.name < b.name) ? 1 : -1);
     }
   }
-
 // Fonction pour obtenir les éléments de la page courante, slice pour retirer les éléments non nécéssaires
 get items(): Item[] {
   let filteredData = this.data;
   if (this.selectedStudios.length > 0) {
-      filteredData = this.data.filter((item:Item) => this.selectedStudios.includes(item.studio));
+      filteredData = filteredData.filter((item:Item) => this.selectedStudios.includes(item.studio));
+  } 
+  if (this.selectedYears.length > 0) {
+      filteredData = filteredData.filter((item:Item) => this.selectedYears.includes(item.year));
+  } 
+  if (this.selectedSupports.length > 0){
+      filteredData = filteredData.filter((item:Item) => this.selectedSupports.includes(item.support));
   }
   const start = (this.currentPage - 1) * this.itemsPerPage;
   return filteredData.slice(start, start + this.itemsPerPage);
 }
-
-
-//filtrer
-filterBySelectedStudio(studio: string) {
-  this.filterByStudio = studio;
-  this.currentPage = 1;
-}
-
-//fonction  pour mettre à jour le tableau des studios sélectionnés lorsqu'une case est cochée et décochée
+//Filtrer : fonction  pour mettre à jour le tableau des studios sélectionnés lorsqu'une case est cochée et décochée
 updateSelectedStudios(studio: string, checkedInput: HTMLInputElement) {
   if (checkedInput.checked) {
       this.selectedStudios.push(studio);
@@ -85,9 +94,23 @@ updateSelectedStudios(studio: string, checkedInput: HTMLInputElement) {
   this.currentPage = 1;
 }
 
+updateSelectedYears(year: string, checkedInput: HTMLInputElement) {
+  if (checkedInput.checked) {
+      this.selectedYears.push(year);
+  } else {
+      this.selectedYears = this.selectedYears.filter(s => s !== year);
+  }
+  this.currentPage = 1;
+}
 
-
-
+updateSelectedSupports(support: string, checkedInput: HTMLInputElement) {
+  if (checkedInput.checked) {
+      this.selectedSupports.push(support);
+  } else {
+      this.selectedSupports = this.selectedSupports.filter(s => s !== support);
+  }
+  this.currentPage = 1;
+}
 // Fonction pour aller à la page suivante
   nextPage() {
     this.currentPage++;
