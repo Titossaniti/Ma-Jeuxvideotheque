07699bcd1153dc802e8e51c18daea1d91a53ad6b
Titossaniti <tristan.erard@gmail.com>
2023-01-27 12:19:47 +0100

    Checkbox input styled thx to open source code

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -16,7 +16,7 @@ export class HeaderComponent implements OnInit{
       this.apiImages = data;
     });
   }
-
+//fonction pour mélanger l'ordre des images dans le carrousel
   shuffleArray(array:any) {
     for (let i = array.length - 1; i > 0; i--) {
         const j = Math.floor(Math.random() * (i + 1));
