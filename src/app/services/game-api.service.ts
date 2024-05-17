9439fff4e10df233b74b1d2176743efd20e6428b
Titossaniti <tristan.erard@gmail.com>
2024-05-17 14:36:52 +0200

    api updated

--- src/app/services/game-api.service.ts
+++ src/app/services/game-api.service.ts
@@ -15,7 +15,9 @@ interface Game {
   desc: string;
 }
 
-
+@Injectable({
+  providedIn: 'root'
+})
 export class GameAPIService {
   private games: Game[] = [];
   private baseUrl = window.location.origin;
@@ -23,14 +25,14 @@ export class GameAPIService {
   constructor(private http: HttpClient) {}
 
   getDataFromApi() {
-    return this.http.get('https://titossaniti.github.io/api-jeux/data.json');
+    return this.http.get('https://quaint-tan-bull.cyclic.app/api/movies');
   }
   getImages() {
-    return this.http.get<any[]>('https://titossaniti.github.io/api-jeux/data.json');
+    return this.http.get<any[]>('https://quaint-tan-bull.cyclic.app/api/movies');
   }
   getDataForPage(gameName: string): Observable<any> {
     const url = `${this.baseUrl}?image=${gameName}`;
-    return this.http.get<any[]>('https://titossaniti.github.io/api-jeux/data.json');
+    return this.http.get<any[]>('https://quaint-tan-bull.cyclic.app/api/movies');
   }
 
 //filtre et tri
